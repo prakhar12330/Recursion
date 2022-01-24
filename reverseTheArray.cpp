5a50ecfae9c143d2c40e2229a706a1fe9835e82f
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-24 15:52:41 +0530

    Add files via upload

--- reverseTheArray.cpp
+++ reverseTheArray.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-void reverseArray(int arr[],int l,int r)
+void reverseArray(int arr[],int i,int n)
 {
-   if(l>=r)return;
-   swap(arr[l],arr[r]);
-   reverseArray(arr,l+1,r-1);
+   if(i>=n/2)return;
+   swap(arr[i],arr[n-i-1]);
+   reverseArray(arr,i+1,n);
 
 }
 
@@ -14,14 +14,14 @@ int main()
 {
   int n=5;
   int Arr[5]={2,7,4,9,1};
-  int l=0,r=n-1;
+  int a=0;
   cout << "Array Before Reversing :";
   for (int i = 0; i < n; i++)
   {
       cout << Arr[i] << " ";
   }
 
-  reverseArray(Arr,l,r);
+  reverseArray(Arr,a,n);
   cout<<endl;
   cout<<"Array After Reversing :";
   for(int i=0;i<n;i++)
