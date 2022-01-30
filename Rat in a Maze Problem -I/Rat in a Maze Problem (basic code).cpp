ec2eeca119c85011c4e52741e4f07692f9854259
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-30 16:20:11 +0530

    Create Rat in a Maze Problem (basic code).cpp

---
+++ Rat in a Maze Problem -I/Rat in a Maze Problem (basic code).cpp
@@ -0,0 +1,56 @@
+class Solution{
+    public:
+    void solve(int i,int j,vector<vector<int>> &m,int n,vector<string> &ans,string move,vector<vector<int>> &vis)
+    {
+       if(i==n-1 && j==n-1)
+       {
+           ans.push_back(move);
+           return;
+       }
+      
+        //Lexiographically order -->DLRU
+        
+        //Condition for downward movement 
+        if(i+1<n && !vis[i+1][j] && m[i+1][j]==1)
+        {
+            vis[i][j]=1;
+            solve(i+1,j,m,n,ans,move+'D',vis);
+            vis[i][j]=0;        
+            
+        }
+        //Condition for left movement 
+        if(j-1>=0 && !vis[i][j-1] && m[i][j-1]==1)
+        {
+            vis[i][j]=1;
+            solve(i,j-1,m,n,ans,move+'L',vis);
+            vis[i][j]=0;        
+            
+        }
+        //Condition for right movement 
+        if(j+1<n && !vis[i][j+1] && m[i][j+1]==1)
+        {
+            vis[i][j]=1;
+            solve(i,j+1,m,n,ans,move+'R',vis);
+            vis[i][j]=0;        
+            
+        }
+        //Condition for upward movement 
+        if(i-1>=0 && !vis[i-1][j] && m[i-1][j]==1)
+        {
+            vis[i][j]=1;
+            solve(i-1,j,m,n,ans,move+'U',vis);
+            vis[i][j]=0;        
+            
+        }
+      
+        
+    }
+     vector<string> findPath(vector<vector<int>> &m, int n) 
+    {
+        // Your code goes here
+        vector<string>ans; //string vector to store the ans
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        if(m[0][0] == 1)solve(0,0,m,n,ans,"",vis);
+        return ans;
+    }
+};
